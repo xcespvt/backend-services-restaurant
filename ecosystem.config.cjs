5024c20f3ff22c3a1bf9e88dc6bdcf5eb7d31514
Chintan <xcespvt@gmail.com>
2025-10-15 03:09:50 +0530

    updated ecosystem fiile

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,12 +4,12 @@ module.exports = {
   apps: [{
     name: "app",
     script: "index.js",
-    interpreter: "bun",
+    interpreter: "node",
     env: {
       /**
        * This is your full system PATH, hardcoded.
-       * Since '/home/rocky/.bun/bin' is already at the beginning, 
-       * the system will find the 'bun' interpreter without any issues.
+       * Since '/home/rocky/.nvm/versions/node/v24.9.0/bin' is in the PATH, 
+       * the system will find the 'node' interpreter without any issues.
        */
       PATH: "/home/rocky/.bun/bin:/home/rocky/.nvm/versions/node/v24.9.0/bin:/home/rocky/.local/bin:/home/rocky/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin",
     },
